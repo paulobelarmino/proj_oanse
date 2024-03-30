57addc2c5090bd40d22487870f2ad13f7026e6f4
eve4t00n <andradeeverton354@gmail.com>
2024-03-30 00:16:08 -0300

    feat: adiciona a rota Lideres, ejs e bcrypt

--- src/backend/src/routes/Lideres/index.js
+++ src/backend/src/routes/Lideres/index.js
@@ -1,11 +1,8 @@
 const express = require('express')
 const router = express.Router()
-const lideresRoute = require('./Lideres/index')
 
 router.get('/',(req,res) => {
-    res.render('index')
+    res.render('Lideres/index')
 })
-
-router.use('/lideres',lideresRoute)
 
 module.exports = router
--- src/backend/src/server.js
+++ src/backend/src/server.js
@@ -2,9 +2,10 @@ const express = require('express')
 const app = express()
 const indexRoute = require('./routes/index.js')
 
-app.use(express.urlencoded({extends:true}))
+app.use(express.urlencoded({extended:true}))
 app.use(express.json())
-app.use('/index',indexRoute)
+app.set('view engine','ejs')
+app.use('/',indexRoute)
 
 app.listen(3000, () => {
     console.log('Servidor Rodando')
