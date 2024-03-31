2dbf55551f544cdf5ed2ea3e97f656c63fe28460
eve4t00n <andradeeverton354@gmail.com>
2024-03-30 23:30:30 -0300

    fix: adiciona campos em falta email e senha

--- src/backend/migrations/20240330022742_create_lideres.js
+++ src/backend/migrations/20240330022742_create_lideres.js
@@ -2,6 +2,8 @@ exports.up = function(knex) {
   return knex.schema.createTable('lideres',(table) => {
     table.increments('id').primary()
     table.text('nome')
+    table.text('email')
+    table.text('senha')
     table.integer('contato')
     table.integer('id_endereco')
     table.integer('id_funcao')
